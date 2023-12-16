0001c088dfce709a4066af72be940cb74d2a1219
Mauro Harmitton <mauro.harmittonbr@gmail.com>
2023-12-15 23:28:08 -0300

    last commit

--- src/app/app.js
+++ src/app/app.js
@@ -3,9 +3,6 @@ import path from 'path';
 import handlebars from 'express-handlebars';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import passport from 'passport';
-import LocalStrategy from 'passport-local';
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { init } from './socket.servidor.js';
@@ -40,9 +37,16 @@ app.use('/products', productRouter);
 app.engine('handlebars', handlebars.engine());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
-app.use(session({ secret: COOKIE_SECRET, resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+});
+
+app.use((error, req, res, next) => {
+    const message = `Ah ocurrido un error desconocido.. ${error.message}`;
+    console.error(message);
+    res.status(500).json({ message });
+});
 
 mongoose.connect("mongodb+srv://mauroharmitton:Password1@cluster0.453yel4.mongodb.net/Users?retryWrites=true&w=majority", {
     useNewUrlParser: true,
@@ -68,50 +72,67 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
 
 const User = mongoose.model('User', userSchema);
 
-passport.use(new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
-        try {
-            const user = await User.findOne({ email });
-
-            if (!user) {
-                return done(null, false, { message: 'Usuario no encontrado' });
-            }
-
-            const isPasswordValid = await user.comparePassword(password);
-
-            if (!isPasswordValid) {
-                return done(null, false, { message: 'Contraseña incorrecta' });
-            }
-
-            return done(null, user);
-        } catch (error) {
-            return done(error);
-        }
-    }
-));
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error);
-    }
-});
-
 // Ruta de registro
-app.post('/register', passport.authenticate('register', {
-    successRedirect: '/login',
-    failureRedirect: '/register',
-    failureFlash: true,
-}));
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
 
-// Resto del código...
+    try {
+        // Verificar si el usuario ya existe en la base de datos
+        const existingUser = await User.findOne({ email });
+        console.log(existingUser);
+        if (existingUser) {
+            return res.status(400).json({ message: 'El email ya existe' });
+        }
+
+        // Hashear la contraseña antes de almacenarla en la base de datos
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Crear un nuevo usuario
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+        });
+
+        // Guardar el nuevo usuario en la base de datos
+        await User.create(newUser);
+
+        res.status(201).redirect('/login');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error en el servidor' });
+    }
+});
+
+// Ruta de inicio de sesión
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Buscar el usuario en la base de datos
+        const user = await User.findOne({ email });
+
+        // Verificar si el usuario existe
+        if (!user) {
+            return res.status(404).json({ message: 'Email no encontrado' });
+        }
+
+        // Verificar la contraseña
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Contraseña incorrecta' });
+        }
+
+        // Aquí puedes establecer una sesión o generar un token de autenticación
+        // Por ejemplo, podrías usar la biblioteca jsonwebtoken para generar un token
+
+        // ...
+
+        res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error en el servidor' });
+    }
+});
 
 const httpServer = app.listen(8080, () => {
     console.log('Server ON PORT 8080');
